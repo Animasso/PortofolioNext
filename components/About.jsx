7f7a9b0122c35b09137dac74fd55f6802cea69ab
Animasso Sidibe <asidibe017@gmail.com>
2023-03-29 06:43:35 +0200

    deploy

--- components/About.jsx
+++ components/About.jsx
@@ -14,24 +14,26 @@ const About = () => {
           </p>
           <h2 className="py-4"> Qui suis-je ?</h2>
           <p className="py-2 text-gray-600">
-            Titulaire d'un diplôme bac +3/4 Développeur concepteur logiciel,
-            j'aime le processus de création qui est lié au Front-end ,créer des
-            applications qui seront facilement compréhensibles et manipulables
-            par un utilisateur. Ce qui me plaît dans le Frontend c'est la
-            possibilité de matérialiser différentes interfaces grâce la maîtrise
-            d'outils comme JavaScript ou encore React. Je me suis lancé dans une
-            carrière dans le web car ce métier laisse place à l'imagination
-            partir de zéro à une page dynamique où l'utilisateur peut se
-            connecter tout en aillant un plaisir visuel est une navigation
-            intuitive. Je suis de nature curieuse ce qui me permet d'avoir des
-            centres d'intérêt très différents, j'aime apprendre et relever de
+            Titulaire d&apos;un diplôme bac +3/4 Développeur concepteur
+            logiciel, j&apos;aime le processus de création qui est lié au
+            Front-end ,créer des applications qui seront facilement
+            compréhensibles et manipulables par un utilisateur. Ce qui me plaît
+            dans le Frontend c&apos;est la possibilité de matérialiser
+            différentes interfaces grâce la maîtrise d&apos;outils comme
+            JavaScript ou encore React. Je me suis lancé dans une carrière dans
+            le web car ce métier laisse place à l&apos;imagination partir de
+            zéro à une page dynamique où l&apos;utilisateur peut se connecter
+            tout en aillant un plaisir visuel est une navigation intuitive. Je
+            suis de nature curieuse ce qui me permet d&apos;avoir des centres
+            d&apos;intérêt très différents, j&apos;aime apprendre et relever de
             nouveaux challenges. Dans un domaine où les technologies évoluent
-            rapidement et apportent toujours plus de puissance et d'innovation
-            la veille technologique que j'ai mis en place me permet de garder un
-            oeil sur les mises à jour et les nouvelles avancées. Mes différentes
-            expériences professionnelles m'ont permis acquérir une bonne
-            communication et de la rigueur ce qui est essentiel pour un bon
-            travail en équipe lors de la création d'un projet en entreprise.
+            rapidement et apportent toujours plus de puissance et
+            d&apos;innovation la veille technologique que j&apos;ai mis en place
+            me permet de garder un oeil sur les mises à jour et les nouvelles
+            avancées. Mes différentes expériences professionnelles m&apos;ont
+            permis acquérir une bonne communication et de la rigueur ce qui est
+            essentiel pour un bon travail en équipe lors de la création
+            d&apos;un projet en entreprise.
           </p>
           <p className="py-2 cursor-pointer text-gray-600 underline">
             Mes Projets
