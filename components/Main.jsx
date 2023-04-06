4d0bf75b97f85a87942fd44dce28562f99bde8b5
Animasso Sidibe <asidibe017@gmail.com>
2023-04-06 17:26:54 +0200

    fix

--- components/Main.jsx
+++ components/Main.jsx
@@ -8,7 +8,7 @@ export default function Main() {
   return (
     <div id="home" className="w-full h-screen text-center">
       <div className="max-w-[1240px] w-full h-full mx-auto p-2 flex justify-center items-center">
-        <header className="background-lax">
+        <header>
           <div className="sansFont mt-6">
             <span
               id="text"
